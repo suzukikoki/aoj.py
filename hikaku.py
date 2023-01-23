9b8dc723be2ad5bb99f9fd1963278352a5f2eec6
suzukikoki <114800671+suzukikoki@users.noreply.github.com>
2023-01-23 07:21:48 +0000

    hikaku

---
+++ hikaku.py
@@ -0,0 +1,14 @@
+# import sys
+
+# a,b = map(int,input().split())
+
+# op = ''
+a = input()
+b = input()
+
+if a < b:
+    print("a < b")
+elif a > b:
+    print("a > b")
+else:
+    print("a == b")
